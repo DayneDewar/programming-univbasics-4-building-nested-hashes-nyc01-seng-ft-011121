c4d2b8c35b468a2605f1f6bf4dcf52094ed6bfe6
Dayne Dewar <daynedewar@yahoo.com>
2020-12-28 04:54:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,7 +7,7 @@ end
 def monopoly_with_second_tier
   monopoly = {
     :railroads => {
-      :pieces => 4, :rent_in_dollars => {
+      :pieces => 4, :rent_in_dollars => {:one_piece_owned => 25
         
       }, :names => {}
       
